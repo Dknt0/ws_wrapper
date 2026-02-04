b83e9a0d1cf2989d15aec61d12f199fb4f684d8c
dknt <963725175@qq.com>
2026-02-04 13:15:38 +0800

    update

--- src/twist2ws.cc
+++ src/twist2ws.cc
@@ -4,6 +4,7 @@
 
 #include <geometry_msgs/msg/twist.hpp>
 #include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/int32.hpp>
 
 #include "utils/js_temp.hpp"
 
@@ -20,7 +21,8 @@ class TwistToWebSocketNode : public rclcpp::Node {
     web_socket_.start();
 
     twist_subscription_ = this->create_subscription<geometry_msgs::msg::Twist>(
-        "twist", 10, [this](const geometry_msgs::msg::Twist::SharedPtr msg) {
+        "/robot_cmd_vel", 10,
+        [this](const geometry_msgs::msg::Twist::SharedPtr msg) {
           // RCLCPP_INFO(this->get_logger(),
           //             "Received Twist - Linear: [x: %.2f, y: %.2f, z: %.2f],
           //             " "Angular: [x: %.2f, y: %.2f, z: %.2f]",
@@ -31,11 +33,33 @@ class TwistToWebSocketNode : public rclcpp::Node {
           web_socket_.send(VelocityMsg("set_vel_y", msg->linear.y));
           web_socket_.send(VelocityMsg("set_vel_w", msg->angular.z));
         });
+    int_subscription_ = this->create_subscription<std_msgs::msg::Int32>(
+        "/robot_action", 10, [this](const std_msgs::msg::Int32::SharedPtr msg) {
+          if (msg->data == 2) {
+            web_socket_.send(ButtonMsg<"enable_record", "2">().value);
+            web_socket_.send(ButtonMsg<"power_on", "1">().value);
+            web_socket_.send(ButtonMsg<"start", "2">().value);
+            RCLCPP_INFO(this->get_logger(), "Sent power_on and start commands");
+          } else if (msg->data == 3) {
+            web_socket_.send(ButtonMsg<"init_pose", "2">().value);
+            RCLCPP_INFO(this->get_logger(), "Sent init_pos commands");
+          } else if (msg->data == 4) {
+            web_socket_.send(ButtonMsg<"run_policy", "2">().value);
+            RCLCPP_INFO(this->get_logger(), "Sent run_policy commands");
+          } else if (msg->data == 1) {
+            web_socket_.send(ButtonMsg<"enable_warking_policy", "2">().value);
+            RCLCPP_INFO(this->get_logger(),
+                        "Sent enable_warking_policy commands");
+          } else if (msg->data == 0) {
+            web_socket_.send(ButtonMsg<"stop", "1">().value);
+          }
+        });
   }
 
  private:
   rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr
       twist_subscription_;
+  rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr int_subscription_;
   ix::WebSocket web_socket_;
   std::string url_ = "ws://127.0.0.1:12888/console";
 };
